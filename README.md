d230274ff3ed153bf26fb92f5955ca68348f9128
Pawel Celejewski <s15799@pjwstk.edu.pl>
2021-06-05 12:03:07 +0200

    fixed zapytanie_4_MR , min and max BMI calculated properly
    Signed-off-by: Pawel Celejewski <s15799@pjwstk.edu.pl>

--- README.md
+++
@@ -1,5 +0,0 @@
-# NBD_04
-NBD_04 Mongo Aggregation and MapReduce
-
-Wszystkie wyniki zapytań są zgodne z oczekiwaniami z wyjątkiem wyników zapytania w pliku zapytanie_4_MR.js
-Srednia wylicza się prawidłowo, lecz nie wartosci min i max. 
--- zapytanie_4_MR.js
+++ zapytanie_4_MR.js
@@ -8,7 +8,7 @@ var mapFunction4 = function() {
 
 	const value = {
 		count: 1,
-		bmi: bmi,
+		sumBMI: bmi,
 		minBMI: bmi,
 		maxBMI: bmi
 	}
@@ -18,26 +18,24 @@ var mapFunction4 = function() {
 
 var reduceFunction4 = function(nationality, values) {
 
-	const initValue = values[0].bmi
-
 	var reducedValues = {
-		count: 0,
-		bmi: 0, // sum of BMI
-		minBMI: initValue,
-		maxBMI: initValue
+		count: values[0].count,
+		sumBMI: values[0].sumBMI,
+		minBMI: values[0].minBMI,
+		maxBMI: values[0].maxBMI
 	}
 
 	for (var idx = 0; idx < values.length; idx++){
 
 		reducedValues.count = reducedValues.count + values[idx].count;
-		reducedValues.bmi = reducedValues.bmi + values[idx].bmi;
+		reducedValues.sumBMI = reducedValues.sumBMI + values[idx].sumBMI;
 
-		if (reducedValues.minBMI >  values[idx].bmi){
-			reducedValues.minBMI = values[idx].bmi;
+		if (reducedValues.minBMI >  values[idx].minBMI){
+			reducedValues.minBMI = values[idx].minBMI;
 		}
 
-		if (reducedValues.maxBMI <  values[idx].bmi){
-			reducedValues.maxBMI = values[idx].bmi;
+		if (reducedValues.maxBMI <  values[idx].maxBMI){
+			reducedValues.maxBMI = values[idx].maxBMI;
 		}
 	}
 
@@ -47,7 +45,7 @@ var reduceFunction4 = function(nationality, values) {
 
 var finalizeFunction4 = function(nationality, reducedValues) {
 
-	var avgBMI = reducedValues.bmi / reducedValues.count;
+	var avgBMI = reducedValues.sumBMI / reducedValues.count;
 
 	var finalizedValues = {
 		count: reducedValues.count,
@@ -64,8 +62,8 @@ printjson(db.people.mapReduce(
    mapFunction4,
    reduceFunction4,
    {
-   	out: "map_reduce_output_4",
+   	out: "map_reduce_output_45",
 	finalize: finalizeFunction4}
 ))
 
-printjson(db.map_reduce_output_4.find({}).toArray())
+printjson(db.map_reduce_output_45.find({}).toArray())
